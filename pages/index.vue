7f4857e64b58fe8763d02c6d01522c8a64deef68
sandiprb <baradiya.sandip@gmail.com>
2021-05-02 18:09:34 +0530

    Init

---
+++ pages/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container mx-auto text-center">
+    <div>
+      <Logo />
+      <h1 class="text-purple">
+        cowin-alert-frontend
+      </h1>
+      <div class="links">
+        <a
+          href="https://nuxtjs.org/"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--green"
+        >
+          Documentation
+        </a>
+        <a
+          href="https://github.com/nuxt/nuxt.js"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="button--grey"
+        >
+          GitHub
+        </a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+    colors :{
+      purple: "#4255FE",
+    }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
