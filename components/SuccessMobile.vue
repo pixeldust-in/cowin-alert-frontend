64d7c616257d8ff7ae4744529b9252f79abffe5d
UI-Pixeldust <ui@pixeldust.in>
2021-05-03 00:28:49 +0530

    ui added

--- components/SuccessMobile.vue
+++ components/SuccessMobile.vue
@@ -3,10 +3,10 @@
     <div class="mx-auto w-full pt-14 px-8 bg-purple rounded-xl">
       <div class="text-white text-2xl font-bold">Registered Successfully!</div>
       <div class="text-white mt-5">
-        We're in this together. Let's get<br />
+        We're in this together.<br /> Let's get
         vaccinated at the earliest!
       </div>
-      <div class="mt-10 flex justify-center">
+      <div class="flex mt-14 justify-center">
         <img src="@/assets/images/success-message.svg" />
       </div>
     </div>
